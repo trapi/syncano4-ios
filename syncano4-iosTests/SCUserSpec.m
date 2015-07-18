2d37c4d1eb586db8de8cdb0169174dd10e58f5c8
Jan Lipmann <jan.lipmann@likomp.pl>
2015-07-18 06:59:56 +0200

    all SCUser tests

--- syncano4-iosTests/SCUserSpec.m
+++ syncano4-iosTests/SCUserSpec.m
@@ -7,14 +7,149 @@
 //
 
 #import "Kiwi.h"
-#import "SCUser.h"
 #import "KWSpec+WaitFor.h"
-#import "Syncano.h"
 #import <OHHTTPStubs.h>
+#import "Syncano.h"
+#import "SCUser.h"
+#import "CustomUserProfile.h"
+#import "SCPlease.h"
 
 SPEC_BEGIN(SCUserSpec)
 
 describe(@"SCUser", ^{
+    
+    context(@"singleton syncano instance", ^{
+        beforeAll(^{
+            [Syncano sharedInstanceWithApiKey:@"API-KEY" instanceName:@"INSTANCE-NAME"];
+        });
+        
+        it(@"should register", ^{
+            
+            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                return YES;
+            } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+                // Stub it with our "wsresponse.json" stub file
+                return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFile(@"SCUserRegister_200.json",self.class)
+                                                        statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+            }];
+            
+            __block NSError *_error;
+            __block BOOL _blockFinished;
+            __block SCUser *registereddUser;
+            [SCUser registerWithUsername:@"zenon" password:@"rtw57hwggwt6" completion:^(NSError *error) {
+                _error = error;
+                _blockFinished = YES;
+                registereddUser = [SCUser currentUser];
+                //[SCCannedURLProtocol unregisterFromAPIClient:syncano.apiClient];
+                
+            }];
+            [[expectFutureValue(theValue(_blockFinished)) shouldEventually] beYes];
+            [[_error shouldEventually] beNil];
+            [[registereddUser shouldEventually] beNonNil];
+        });
+        
+        
+        
+        it(@"should login and logout", ^{
+            
+            //Login
+            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                return YES;
+            } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+                return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFile(@"SCUserLogin_200.json",self.class)
+                                                        statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+            }];
+            
+            __block SCUser *loggedUser;
+            __block NSError *_error;
+            __block BOOL _blockFinished;
+            [SCUser loginWithUsername:@"janek" password:@"qaz123" completion:^(NSError *error) {
+                _error = error;
+                _blockFinished = YES;
+                loggedUser = [SCUser currentUser];
+            }];
+            
+            [[expectFutureValue(theValue(_blockFinished)) shouldEventually] beYes];
+            [[_error shouldEventually] beNil];
+            [[loggedUser shouldEventually] beNonNil];
+            [[[loggedUser userKey] shouldEventually] beNonNil];
+            [[[loggedUser username] shouldEventually] beNonNil];
+            
+            //Log out
+            [loggedUser logout];
+            [[[loggedUser userKey] should] beNil];
+        });
+        
+        it(@"should update username", ^{
+            __block SCUser *loggedUser;
+            __block NSError *_error;
+            __block BOOL _blockFinished;
+            
+            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                return YES;
+            } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+                return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFile(@"SCUserLogin_200.json",self.class)
+                                                        statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+            }];
+            
+            [SCUser loginWithUsername:@"janek" password:@"qaz123" completion:^(NSError *error) {
+                _error = error;
+                if(error) {
+                    _blockFinished = YES;
+                } else {
+                    loggedUser = [SCUser currentUser];
+                    [loggedUser updateUsername:@"marek" withCompletion:^(NSError *error) {
+                        _error = error;
+                        _blockFinished = YES;
+                    }];
+                }
+            }];
+            
+            
+            [[expectFutureValue(theValue(_blockFinished)) shouldEventually] beYes];
+            [[_error shouldEventually] beNil];
+            [[loggedUser shouldEventually] beNonNil];
+            [[loggedUser.username shouldEventually] equal:@"marek"];
+        });
+        
+        it(@"should update password", ^{
+            __block SCUser *loggedUser;
+            __block NSError *_error;
+            __block BOOL _blockFinished;
+            
+            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                return YES;
+            } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+                return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFile(@"SCUserLogin_200.json",self.class)
+                                                        statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+            }];
+            
+            [SCUser loginWithUsername:@"janek" password:@"qaz123" completion:^(NSError *error) {
+                _error = error;
+                if(error) {
+                    _blockFinished = YES;
+                } else {
+                    loggedUser = [SCUser currentUser];
+                    [loggedUser updatePassword:@"marek" withCompletion:^(NSError *error) {
+                        _error = error;
+                        _blockFinished = YES;
+                    }];
+                }
+            }];
+            
+            
+            [[expectFutureValue(theValue(_blockFinished)) shouldEventually] beYes];
+            [[_error shouldEventually] beNil];
+        });
+
+        it(@"should return SCPlease instance", ^{
+            SCPlease *please = [SCUser please];
+            [[please shouldNot] beNil];
+            [[please should] beKindOfClass:[SCPlease class]];
+        });
+        
+    });
+
     
     context(@"custom syncano instance", ^{
         Syncano *syncano = [[Syncano alloc] initWithApiKey:@"API-KEY" instanceName:@"INSTANCE-NAME"];
@@ -76,12 +211,137 @@ describe(@"SCUser", ^{
             [[[loggedUser userKey] should] beNil];
         });
         
+        it(@"should update username", ^{
+            __block SCUser *loggedUser;
+            __block NSError *_error;
+            __block BOOL _blockFinished;
+            
+            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                return YES;
+            } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+                return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFile(@"SCUserLogin_200.json",self.class)
+                                                        statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+            }];
+            
+            [SCUser loginWithUsername:@"janek" password:@"qaz123" toSyncano:syncano completion:^(NSError *error) {
+                _error = error;
+                if(error) {
+                    _blockFinished = YES;
+                } else {
+                    loggedUser = [SCUser currentUser];
+                    [loggedUser updateUsername:@"marek" inSyncano:syncano withCompletion:^(NSError *error) {
+                        _error = error;
+                        _blockFinished = YES;
+                    }];
+                }
+            }];
+            
+            
+            [[expectFutureValue(theValue(_blockFinished)) shouldEventually] beYes];
+            [[_error shouldEventually] beNil];
+            [[loggedUser shouldEventually] beNonNil];
+            [[loggedUser.username shouldEventually] equal:@"marek"];
+        });
 
+        it(@"should update password", ^{
+            __block SCUser *loggedUser;
+            __block NSError *_error;
+            __block BOOL _blockFinished;
+            
+            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                return YES;
+            } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+                return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFile(@"SCUserLogin_200.json",self.class)
+                                                        statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+            }];
+            
+            [SCUser loginWithUsername:@"janek" password:@"qaz123" toSyncano:syncano completion:^(NSError *error) {
+                _error = error;
+                if(error) {
+                    _blockFinished = YES;
+                } else {
+                    loggedUser = [SCUser currentUser];
+                    [loggedUser updatePassword:@"marek" inSyncno:syncano withCompletion:^(NSError *error) {
+                        _error = error;
+                        _blockFinished = YES;
+                    }];
+                }
+            }];
+            
+            
+            [[expectFutureValue(theValue(_blockFinished)) shouldEventually] beYes];
+            [[_error shouldEventually] beNil];
+        });
+
+        it(@"should return SCPlease instance", ^{
+            SCPlease *please = [SCUser pleaseFromSyncano:syncano];
+            [[please shouldNot] beNil];
+            [[please should] beKindOfClass:[SCPlease class]];
+        });
+    });
+    
+    context(@"custom user profile", ^{
+        it(@"should register class", ^{
+            __block NSString *customProfileClassName;
+            __block BOOL _blockFinished;
+            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                return YES;
+            } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+                return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFile(@"SCUserLogin_200.json",self.class)
+                                                        statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+            }];
+            [SCUser registerClassWithProfileClass:[CustomUserProfile class]];
+            [[SCUser currentUser] logout];
+            [SCUser loginWithUsername:@"janek" password:@"qaz123" completion:^(NSError *error) {
+                SCUser *user = [SCUser currentUser];
+                customProfileClassName = NSStringFromClass(user.profile.class);
+                _blockFinished = YES;
+                
+            }];
+            [[expectFutureValue(theValue(_blockFinished)) shouldEventually] beYes];
+            [[customProfileClassName should] equal:@"CustomUserProfile"];
+        });
     });
     
     
-    
-    
+    context(@"default user profile", ^{
+        it(@"should register class", ^{
+            
+            __block NSString *profileClassName;
+            __block BOOL _blockFinished;
+            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                return YES;
+            } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+                return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFile(@"SCUserLogin_200.json",self.class)
+                                                        statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+            }];
+            [SCUser registerClass];
+            [[SCUser currentUser] logout];
+            [SCUser loginWithUsername:@"janek" password:@"qaz123" completion:^(NSError *error) {
+                SCUser *user = [SCUser currentUser];
+                profileClassName = NSStringFromClass(user.profile.class);
+                _blockFinished = YES;
+                
+            }];
+            [[expectFutureValue(theValue(_blockFinished)) shouldEventually] beYes];
+            [[profileClassName should] equal:@"SCUserProfile"];
+            
+        });
+    });
+
+    it(@"should fill object with JSON dictionary", ^{
+        NSDictionary *JSONDictioonary = @{@"id" : @12,
+                                          @"username" : @"janek",
+                                          @"links" : @{@"profile":@"/v1/instances/mytestinstance/classes/user_profile/objects/85/",@"self":@"/v1/instances/mytestinstance/users/1/",@"groups":@"/v1/instances/mytestinstance/users/1/groups/",@"reset-key":@"/v1/instances/mytestinstance/users/1/reset_key/"}};
+        
+        SCUser *user = [SCUser new];
+        [user fillWithJSONObject:JSONDictioonary];
+        
+        [[user.userId should] equal:@12];
+        [[user.username should] equal:@"janek"];
+        [[user.links shouldNot] beNil];
+        [[user.links should] beKindOfClass:[NSDictionary class]];
+    });
     
 });
 
